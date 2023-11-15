754068b67a00cb480e4473b4a449a34115bb419b
Mihail Feraru <mihailferaru2000@gmail.com>
2023-11-16 00:43:04 +0200

    upsolve 1881

--- contests/1881/F.cpp
+++ contests/1881/F.cpp
@@ -24,19 +24,58 @@ void prep();
 const uint nmax = 2e5 + 100;
 vector<uint> G[nmax];
 
+uint mark[nmax];
+uint d1[nmax];
+uint d2[nmax];
+
+void dfs1(uint root, uint parent = 0)
+{
+  for (auto next : G[root])
+  {
+    if (next == parent)
+    {
+      continue;
+    }
+    d1[next] = d1[root] + 1;
+    dfs1(next, root);
+  }
+}
+
+void dfs2(uint root, uint parent = 0)
+{
+  for (auto next : G[root])
+  {
+    if (next == parent)
+    {
+      continue;
+    }
+    d2[next] = d2[root] + 1;
+    dfs2(next, root);
+  }
+}
+
 int main()
 {
   prep();
 
-  uint n;
+  uint n, k;
   int t;
   cin >> t;
   while (t--)
   {
-    cin >> n;
+    cin >> n >> k;
     for (uint i = 1; i <= n; i++)
     {
       G[i].clear();
+      mark[i] = 0;
+      d1[i] = d2[i] = 0;
+    }
+
+    for (uint i = 0; i < k; i++)
+    {
+      uint u;
+      cin >> u;
+      mark[i] = u;
     }
 
     for (uint i = 0; i < n - 1; i++)
@@ -46,6 +85,30 @@ int main()
       G[u].push_back(v);
       G[v].push_back(u);
     }
+
+    if (k == 1)
+    {
+      cout << "0\n";
+      continue;
+    }
+
+    dfs1(mark[0]);
+    uint u = 0;
+    for (uint i = 0; i < k; i++)
+    {
+      if (d1[u] < d1[mark[i]])
+        u = mark[i];
+    }
+
+    dfs2(u);
+    uint v = 0;
+    for (uint i = 0; i < k; i++)
+    {
+      if (d2[v] < d2[mark[i]])
+        v = mark[i];
+    }
+
+    cout << (d2[v] / 2 + d2[v] % 2) << "\n";
   }
 
   return 0;
