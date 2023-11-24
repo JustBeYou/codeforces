798f5226d3461ab5a065060f5b5c6ffaa11b3ce5
Mihail Feraru <mihailferaru2000@gmail.com>
2023-11-24 16:30:20 +0200

    1898

---
+++ contests/1898/A.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef signed long long ll;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+template <typename T>
+void print_array(const T v[], uint n);
+template <typename T>
+void print_vector(const vector<T> &v, uint n = 0);
+template <typename T>
+void read_array(T v[], uint n);
+template <typename T>
+void read_array_and_size(T v[], uint &n);
+template <typename T>
+void read_vector(vector<T> &v, uint n = 0);
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n);
+void prep();
+
+const uint nmax = 1e5;
+
+int main()
+{
+  prep();
+
+  uint n, k;
+  string s;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    cin >> n >> k;
+    cin >> s;
+
+    uint a = 0, b = 0;
+    for (auto ch : s)
+    {
+      if (ch == 'A')
+      {
+        a++;
+      }
+      else
+      {
+        b++;
+      }
+    }
+
+    if (b == k)
+    {
+      cout << "0\n";
+      continue;
+    }
+
+    cout << "1\n";
+    uint i = 0;
+    char c;
+
+    if (b < k)
+    {
+      c = 'B';
+    }
+    else
+    {
+      c = 'A';
+    }
+    uint q = b > k ? b - k : k - b;
+
+    for (auto ch : s)
+    {
+      if (c != ch)
+      {
+        q--;
+      }
+
+      i++;
+
+      if (q == 0)
+      {
+        break;
+      }
+    }
+
+    cout << i << " " << c << "\n";
+  }
+
+  return 0;
+}
+
+void prep()
+{
+#ifdef DEBUG
+  freopen("input", "r", stdin);
+#else
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+#endif
+}
+
+template <typename T>
+void print_array(const T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
+
+template <typename T>
+void read_array(T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_array_and_size(T v[], uint &n)
+{
+  cin >> n;
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_vector(vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n)
+{
+  cin >> n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void print_vector(const vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
---
+++ contests/1898/B.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef signed long long ll;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+template <typename T>
+void print_array(const T v[], uint n);
+template <typename T>
+void print_vector(const vector<T> &v, uint n = 0);
+template <typename T>
+void read_array(T v[], uint n);
+template <typename T>
+void read_array_and_size(T v[], uint &n);
+template <typename T>
+void read_vector(vector<T> &v, uint n = 0);
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n);
+void prep();
+
+const uint nmax = 2e5 + 10;
+uint a[nmax];
+
+int main()
+{
+  prep();
+
+  uint n;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    read_array_and_size(a, n);
+
+    uint max_val = a[n - 1];
+    ull sol = 0;
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+      if (a[i] <= max_val)
+      {
+        max_val = a[i];
+      }
+      else if (a[i] % max_val == 0)
+      {
+        sol += a[i] / max_val - 1;
+      }
+      else
+      {
+        sol += a[i] / max_val;
+        max_val = a[i] / (a[i] / max_val + 1);
+      }
+    }
+    cout << sol << "\n";
+  }
+
+  return 0;
+}
+
+void prep()
+{
+#ifdef DEBUG
+  freopen("input", "r", stdin);
+#else
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+#endif
+}
+
+template <typename T>
+void print_array(const T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
+
+template <typename T>
+void read_array(T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_array_and_size(T v[], uint &n)
+{
+  cin >> n;
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_vector(vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n)
+{
+  cin >> n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void print_vector(const vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
