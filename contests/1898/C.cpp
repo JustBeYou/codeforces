969c60c1b7d2287c8c6d014851975daca6c59302
Mihail Feraru <mihailferaru2000@gmail.com>
2023-11-26 16:14:37 +0200

    upsolving

--- contests/1898/C.cpp
+++ contests/1898/C.cpp
@@ -21,14 +21,57 @@ template <typename T>
 void read_vector_and_size(vector<T> &v, uint &n);
 void prep();
 
-const uint nmax = 1e5;
+char v[17][17];
+char h[17][17];
 
 int main()
 {
   prep();
 
-  uint n;
+  for (uint i = 1; i <= 16; i++)
+  {
+    for (uint j = 1; j <= 16; j++)
+    {
+      v[i][j] = h[i][j] = 'R';
+    }
+  }
+  v[1][1] = v[1][2] = 'B';
+
   int t;
+  cin >> t;
+  while (t--)
+  {
+    uint n, m, k;
+    cin >> n >> m >> k;
+
+    if (m + n - 2 > k || (k - n - m) % 2 != 0)
+    {
+      cout << "NO\n";
+      continue;
+    }
+
+    cout << "YES\n";
+    for (uint i = 1; i <= m - 1; i++)
+    {
+      h[1][i] = i % 2 == 0 ? 'B' : 'R';
+    }
+    for (uint i = 1; i <= n - 1; i++)
+    {
+      v[i][m] = (m - 1 + i) % 2 == 0 ? 'B' : 'R';
+    }
+    h[n - 1][m - 1] = h[n][m - 1] = v[n - 1][m];
+    v[n][m - 1] = v[n - 1][m] == 'B' ? 'R' : 'B';
+
+    for (uint i = 1; i <= n; i++)
+    {
+      print_array(h[i] + 1, m - 1);
+    }
+
+    for (uint i = 1; i <= n - 1; i++)
+    {
+      print_array(v[i] + 1, m);
+    }
+  }
 
   return 0;
 }
--- contests/1901/C.cpp
+++ contests/1901/C.cpp
@@ -21,66 +21,6 @@ template <typename T>
 void read_vector_and_size(vector<T> &v, uint &n);
 void prep();
 
-const uint nmax = 2e5 + 10;
-
-bool isPossible(const std::vector<ll> &a, ll k, ll &minX, ll &maxX)
-{
-  minX = std::numeric_limits<ll>::min();
-  maxX = std::numeric_limits<ll>::max();
-
-  for (ll ai : a)
-  {
-    ll lowerBound = 2 * k - ai;
-    ll upperBound = 2 * k - ai + 2;
-
-    minX = max(minX, lowerBound);
-    maxX = min(maxX, upperBound - 1);
-
-    if (minX > maxX)
-    {
-      return false;
-    }
-  }
-  return true;
-}
-
-ll findX(const std::vector<ll> &a)
-{
-  ll minK = std::numeric_limits<ll>::max();
-  ll maxK = std::numeric_limits<ll>::min();
-  for (auto ai : a)
-  {
-    minK = min(ai, minK);
-    maxK = max(ai, maxK);
-  }
-
-  ll result = std::numeric_limits<ll>::min();
-
-  while (minK <= maxK)
-  {
-    ll k = minK + (maxK - minK) / 2;
-    ll minX, maxX;
-    if (isPossible(a, k, minX, maxX))
-    {
-      result = minX;
-      break;
-    }
-    else
-    {
-      if (minX > 2 * k - a[0])
-      {
-        minK = k + 1;
-      }
-      else
-      {
-        maxK = k - 1;
-      }
-    }
-  }
-
-  return result;
-}
-
 int main()
 {
   prep();
@@ -91,48 +31,32 @@ int main()
   {
     uint n;
     cin >> n;
-    vector<ll> a(n);
+    vector<ull> a(n);
     read_vector(a, n);
 
-    vector<ll> sols;
-    bool no_sol = true;
-    while (no_sol)
+    if (n == 1)
     {
-      bool all_same = true;
-      for (uint i = 0; i < n - 1; i++)
-      {
-        if (a[i] != a[i + 1])
-        {
-          all_same = false;
-        }
-      }
-
-      if (all_same)
-      {
-        break;
-      }
-
-      ll x = findX(a);
-
-      if (x != std::numeric_limits<ll>::min())
-      {
-        sols.push_back(x);
-        no_sol = false;
-      }
-      else
-      {
-        for (auto &ai : a)
-        {
-          ai /= 2;
-        }
-        sols.push_back(0);
-      }
+      cout << "0\n";
+      continue;
     }
 
-    cout << sols.size() << "\n";
-    if (sols.size() > 0 && sols.size() <= n)
+    sort(a.begin(), a.end());
+
+    if (a[0] == a[n - 1])
     {
-      print_vector(sols);
+      cout << "0\n";
+      continue;
+    }
+
+    ull sol = (ull)log2(a[n - 1] - a[0]) + 1;
+    cout << sol << "\n";
+    if (sol <= n)
+    {
+      for (ull i = 0; i < sol; i++)
+      {
+        cout << a[0] << " ";
+      }
+      cout << "\n";
     }
   }
 
