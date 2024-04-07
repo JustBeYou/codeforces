fba6dc12abdbf3a05ce5e954867de998b386d926
Mihail Feraru <mihailferaru2000@gmail.com>
2024-04-07 21:20:48 +0300

    practice

---
+++ contests/1929/A.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef signed long long ll;
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+template <typename T>
+void print_array(const T v[], uint n);
+template <typename T>
+void print_vector(const vector<T> &v, uint n = 0);
+template <typename T>
+void read_array(T v[], uint n);
+template <typename T>
+void read_array_and_size(T v[], uint &n);
+template <typename T>
+void read_vector(vector<T> &v, uint n = 0);
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n);
+void prep();
+
+const uint nmax = 1e2 + 10;
+ull a[nmax];
+
+int main()
+{
+  prep();
+
+  uint n;
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    read_array_and_size(a, n);
+    sort(a, a + n);
+    ull sol = 0;
+    for (uint i = 1; i < n; i++)
+    {
+      sol += a[i] - a[i - 1];
+    }
+    cout << sol << "\n";
+  }
+
+  return 0;
+}
+
+void prep()
+{
+#ifdef DEBUG
+  freopen("input", "r", stdin);
+#else
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+#endif
+}
+
+template <typename T>
+void print_array(const T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
+
+template <typename T>
+void read_array(T v[], uint n)
+{
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_array_and_size(T v[], uint &n)
+{
+  cin >> n;
+  for (uint i = 0; i < n; i++)
+    cin >> v[i];
+}
+
+template <typename T>
+void read_vector(vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void read_vector_and_size(vector<T> &v, uint &n)
+{
+  cin >> n;
+  if (n > v.size())
+  {
+    v.resize(n);
+  }
+  for (uint i = 0; i < n; i++)
+  {
+    cin >> v[i];
+  }
+}
+
+template <typename T>
+void print_vector(const vector<T> &v, uint n)
+{
+  n = n == 0 ? v.size() : n;
+  for (uint i = 0; i < n; i++)
+  {
+    cout << v[i] << " ";
+  }
+  cout << "\n";
+}
